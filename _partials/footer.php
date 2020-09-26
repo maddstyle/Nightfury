6630d6b7269fe8371773035db2c6ce22231ef084
maddstyle <michaela.decamara@gmail.com>
2020-09-25 22:51:57 -0400

    added functionality

---
+++ _partials/footer.php
@@ -0,0 +1,34 @@
+<?php
+
+
+?>
+
+<aside class="pre-footer">
+	<div class="container">
+		<p>
+			<strong>Zaujali sme Vás pre nový projekt?</strong>
+			Jednoducho nám zavolajte alebo napíšte.
+		</p>
+
+		<ul class="menu personal">
+			<li><i class="fa fa-phone"></i><a href="<?= $contact_phone ?>"><?= $contact_phone ?></a></li>
+			<li><i class="fa fa-envelope"></i><a href="<?= $contact_email ?>"><?= $contact_email ?></a></li>
+		</ul>
+	</div>
+</aside>
+<footer>
+	<div class="container">
+		<p class="logo">
+			<strong>NIGHT</strong>FURY<i class="fa fa-fire"></i>
+		</p>
+		<p class="author">
+			Návrh a design pripravil <a href="mailto:tomas@cloudfender.com">tomas@cloudfender.com</a>
+		</p>
+		<ul class="menu nav-footer">
+			<li><a href="index.php">Domov</a></li>
+			<li><a href="portfolio.php">Portfólio</a></li>
+			<li><a href="about.php">O nás</a></li>
+			<li><a href="contact.php">Kontakt</a></li>
+		</ul>
+	</div>
+</footer>'
---
+++ branding.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Nightfury</title>
+
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600&amp;subset=latin,latin-ext">
+	<link rel="stylesheet" href="css/font-awesome.css">
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/style.css">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<script src="js/jquery-2.1.1.min.js"></script>
+	<!--[if lt IE 9]>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+<?php include './_partials/header.php' ?>;
+
+	<main>
+		<article>
+			<header class="post-header">
+				<div class="container">
+					<h1 class="post-title">Portfólio našich prác</h1>
+				</div>
+			</header>
+			<div class="post-content">
+				<div class="container">
+
+					<h2 class="text-center">Toto je veta, ktorá je tu napísaná, ktorá má hovoriť o tom, ako veľmi sme šikovní sme v našom portfóliovaní a tvorbe vecí, ktoré tvoríme a taktiež vyrábame.</h2>
+
+					<ul class="menu controls">
+						<li data-from="left"><a href="index.php">Všetko</a></li>
+						<li data-from="right"><a href="web.php">Web</a></li>
+						<li data-from="left" class="selected"><a href="#branding">Branding</a></li>
+						<li data-from="right"><a href="fotografia.php">Fotografia</a></li>
+						<li data-from="left"><a href="video.html">Video</a></li>
+					</ul>
+
+				</div>
+
+				<section class="gallery">
+					<div class="gallery-set container" id="branding">
+						<a href="/img/adventure/image-1.jpg">
+							<img src="img/adventure/thumb-1.jpg" height="192" width="383" alt="the dog the human">
+						</a>
+						<a href="img/adventure/image-2.jpg">
+							<img src="img/adventure/thumb-2.jpg" height="192" width="383" alt="the dog the human">
+						</a>
+						<a href="img/adventure/image-3.jpg">
+							<img src="img/adventure/thumb-3.jpg" height="192" width="383" alt="the dog the human">
+						</a>
+						<a href="img/adventure/image-4.jpg">
+							<img src="img/adventure/thumb-4.jpg" height="192" width="383" alt="the dog the human">
+						</a>
+						<a href="img/adventure/image-5.jpg">
+							<img src="img/adventure/thumb-5.jpg" height="192" width="383" alt="the dog the human">
+						</a>
+						<a href="img/adventure/image-6.jpg">
+							<img src="img/adventure/thumb-6.jpg" height="192" width="383" alt="the dog the human">
+						</a>
+						<a href="img/adventure/image-7.jpg">
+							<img src="img/adventure/thumb-7.jpg" height="192" width="383" alt="the dog the human">
+						</a>
+						<a href="img/adventure/image-8.jpg">
+							<img src="img/adventure/thumb-8.jpg" height="192" width="383" alt="the dog the human">
+						</a>
+					</div>
+				</section>
+			</div>
+		</article>
+	</main>
+
+	<?php include '_partials/footer.php' ?>
+	
+	<!-- scripts -->
+	<script src="js/lightbox.js"></script>
+
+</body>
+</html>
